13b772a4a28bbd84ec6aa81beaa0834b9469c238
Andhika Rahman <andhikarhmnp@gmail.com>
2026-06-22 06:37:25 +0700

    implement binary search algorithm

---
+++ binary_search/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    let arr: Vec<i32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let x = 8;
+
+    match binary_search(&arr, x) {
+        Some(idx) => println!("the index of {x} is {idx}"),
+        None => println!("there is no {x}"),
+    }
+}
+
+fn binary_search(arr: &Vec<i32>, x: i32) -> Option<usize> {
+    let mut left: usize = 0;
+    let mut right: usize = arr.len() - 1;
+
+    while left <= right {
+        let mid: usize = left + (right - left) / 2;
+
+        if arr[mid] < x {
+            left = mid + 1;
+        } else if arr[mid] > x {
+            right = mid - 1;
+        } else {
+            return Some(mid);
+        }
+    }
+
+    None
+}
